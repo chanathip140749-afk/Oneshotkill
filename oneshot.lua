32e9ca353fbd700ee3b643eadd6df22e481b1c81
chanathip140749-afk <chanathip140749@gmail.com>
2025-10-10 09:27:28 +0700

    Update oneshot.lua

--- oneshot.lua
+++ oneshot.lua
@@ -23,7 +23,7 @@ spawn(function()
                         if MonPoz and PlayerPoz and (MonPoz - PlayerPoz).Magnitude <= getgenv().Config["Radius"] then
                             if v.Health < v.MaxHealth then
                                 wait(.1)
-                                v.Health = 0
+                                v.Health = 1
                             end
                         end
                     end
